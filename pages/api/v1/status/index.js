cdd8da7456acaf04502c0013917dccee61ac7ee1
Filipe Castro <filipe.oliveira.castro@gmail.com>
2025-12-29 20:14:45 +0000

    add database and .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   // response.status(200).send("alunos curso .dev");
+  const result = await database.query("SELECT 1 + 1;");
   response.status(200).json({ chave: "aluno acima da média" });
 }
 
