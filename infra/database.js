1f7208530411fd2d5ccf4e9fc53e15653d33b56e
Filipe Castro <filipe.oliveira.castro@gmail.com>
2026-01-07 20:27:09 +0000

    refatoracao database js

--- infra/database.js
+++ infra/database.js
@@ -10,8 +10,15 @@ async function query(queryObject) {
   });
 
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
