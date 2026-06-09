121d73f47f3ec6d60c403907427c66d14753c296
Filipe Castro <filipe.oliveira.castro@gmail.com>
2026-06-09 15:09:29 +0000

    refactor: use next-connect in /migrations and /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,27 +1,12 @@
 import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import controller from "infra/controller.js";
 
 const router = createRouter();
 
 router.get(status);
 
-export default router.handler({
-  onNoMatch: onNoMatchHandler,
-  onError: onErrorHandler,
-});
-
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-  response.status(500).json(publicErrorObject);
-}
-
-function onNoMatchHandler(request, response) {
-  const publicErrorObject = new MethodNotAllowedError();
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
+export default router.handler(controller.errorHandlers);
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
--- tests/integration/api/v1/migrations/other-methods.test.js
+++ tests/integration/api/v1/migrations/other-methods.test.js
@@ -2,5 +2,13 @@ test("Not allowed Methodos to /api/v1/migrations should return 405", async () =>
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "PUT",
   });
-  expect(response.status).toBe(405);
+
+  const responseBody = await response.json();
+
+  expect(responseBody).toEqual({
+    name: "MethodNotAllowedError",
+    message: "Método não é permitido para este endpoint",
+    action: "Verifique se o método HTTP enviado é válido para este endpoint",
+    status_code: 405,
+  });
 });
