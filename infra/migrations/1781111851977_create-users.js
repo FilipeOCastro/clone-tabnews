741e0fc0605bb9da0d23c6b728fc7343c3bfb4a0
Filipe Castro <filipe.oliveira.castro@gmail.com>
2026-06-12 17:52:37 +0000

    fix: add updated_at column at migration file

--- infra/migrations/1781111851977_create-users.js
+++ infra/migrations/1781111851977_create-users.js
@@ -18,12 +18,16 @@ exports.up = (pgm) => {
       unique: true,
     },
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
