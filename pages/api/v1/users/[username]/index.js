ac487a8af5a6715a34f12b4bd857ab4b8d4b732f
Filipe Castro <filipe.oliveira.castro@gmail.com>
2026-06-19 20:51:27 +0000

    feat: add support for PATCH on /api/v1/users/username

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -5,6 +5,7 @@ import user from "models/user.js";
 const router = createRouter();
 
 router.get(gettHandler);
+router.patch(patchtHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -12,4 +13,12 @@ async function gettHandler(request, response) {
   const username = request.query.username;
   const userResult = await user.findOneByUsername(username);
   return response.status(200).json(userResult);
+}
+
+async function patchtHandler(request, response) {
+  const username = request.query.username;
+  const userInputValues = request.body;
+
+  const updatedUser = await user.update(username, userInputValues);
+  return response.status(200).json(updatedUser);
 }
