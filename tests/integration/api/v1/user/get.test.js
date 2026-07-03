a36683fc869571f296f70ff80bfe83e4a5931cd9
Filipe Castro <filipe.oliveira.castro@gmail.com>
2026-07-03 20:22:52 +0000

    refactor: invalidate session cookie on unauthorized error

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -88,6 +88,18 @@ describe("GET /api/v1/user", () => {
         action: "Verifique se o usuário está logado e tente novamente.",
         status_code: 401,
       });
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
+      });
+      
     });
     test("With expired session", async () => {
       jest.useFakeTimers({
@@ -117,6 +129,19 @@ describe("GET /api/v1/user", () => {
         action: "Verifique se o usuário está logado e tente novamente.",
         status_code: 401,
       });
+
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
+      });
+
     });
   });
 });
