8cf0e8adc51b19114a31d63530990b076d595b84
Filipe Castro <filipe.oliveira.castro@gmail.com>
2026-08-06 17:52:48 +0000

    feat: require `read:session` to access `/user`endpoint

--- models/activation.js
+++ models/activation.js
@@ -76,7 +76,10 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activeUserByUserId(userId) {
-  const activatedUser = await user.setFeatures(userId, ["create:session"]);
+  const activatedUser = await user.setFeatures(userId, [
+    "create:session",
+    "read:session",
+  ]);
   return activatedUser;
 }
 
